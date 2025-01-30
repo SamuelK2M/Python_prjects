2afc0fab619ae267d1164bf488c2eaa9ba5959c7
SK2ML <skanyinda78@gmail.com>
2025-01-30 17:27:13 +0200

    working game

--- rock_paper_scisor.py
+++ rock_paper_scisor.py
@@ -1,3 +1,5 @@
+import random
+
 #The game need to ask "RocK, Paper, or scisor
 # 
 # need to type"r/p/ss"
@@ -7,3 +9,32 @@
 # need to enter rules of the game
 # 
 # and #  
+
+emojis = {'r' : '🪨', 'p': '📄', 's': '✂️'}
+choices = ('r','p','s')
+
+while True:
+    player_choice = input ('Rock, Paper, or scissors ? (R/P/S)').lower()
+    if player_choice not in choices:
+        print ('Invalid choice !')
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print(f'you chose {emojis[player_choice]}')
+    print(f'computer chose {emojis [computer_choice]}')
+
+    if player_choice == computer_choice:
+        print('Tie ')
+    elif (
+        (player_choice == 'r' and computer_choice == 's') or
+        (player_choice == 's' and computer_choice == 'p') or
+        (player_choice == 'p' and computer_choice == 'r')):
+        print('You win !')
+    else:
+        print('you loss !')
+
+    continue_playing = input('play again ? (y/n):').lower()
+    if continue_playing == 'n':
+        print("Thanks for palying !")
+        break
